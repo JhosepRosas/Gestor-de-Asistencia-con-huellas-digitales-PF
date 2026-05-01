ab9d9f1c7ce51abe5c720a135654690c5a4a2424
JordiCA2 <u21232573@utp.edu.pe>
2026-05-01 01:10:14 -0500

    six seven six seven

---
+++ SistemaAsistencia/src/main/java/com/utp/asistencia/SistemaAsistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.utp.asistencia;
+
+/**
+ *
+ * @author Haskell
+ */
+public class SistemaAsistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
